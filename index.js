5cfa8b0f81e100faa2e4e450341309518bdd848d
Newton Migosi <newtonmigosi@gmail.com>
2018-04-18 09:19:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-function 
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget() {
+  return document.querySelector('#nested.target');
+}
+
+function increaseRankBy(n) {
+  var lysts = document.querySelectorAll('')
+}
